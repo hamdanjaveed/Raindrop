adcd3cd1e281ed838e9dc7363b4a32147919e222
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-09-22 00:10:42 -0400

    Deleted all unneeded files

--- Raindrop/RaindropViewController.m
+++ Raindrop/RaindropViewController.m
@@ -7,45 +7,18 @@
 //
 
 #import "RaindropViewController.h"
-#import "RaindropMyScene.h"
 
 @implementation RaindropViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 
     // Configure the view.
     SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
     
-    // Create and configure the scene.
-    SKScene * scene = [RaindropMyScene sceneWithSize:skView.bounds.size];
-    scene.scaleMode = SKSceneScaleModeAspectFill;
+    // TODO: Create and configure the scene.
     
-    // Present the scene.
-    [skView presentScene:scene];
-}
-
-- (BOOL)shouldAutorotate
-{
-    return YES;
-}
-
-- (NSUInteger)supportedInterfaceOrientations
-{
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return UIInterfaceOrientationMaskAllButUpsideDown;
-    } else {
-        return UIInterfaceOrientationMaskAll;
-    }
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
+    // TODO: Present the scene.
 }
 
 @end
